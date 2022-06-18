1f222766f84c22d42c06997fb176306d5b57ab00
Jungma1 <mbjun3630@naver.com>
2022-06-18 22:06:51 +0900

    영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -16,16 +15,41 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(5)
-                    .getResultList();
+            // 비영속 - 엔티티를 생성한 상태
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            System.out.println("==== BEFORE ====");
+            // 엔티티를 영속성 컨텍스트에 영속
+            // em.persist(member);
+            System.out.println("==== AFTER ====");
 
+            // DB가 아닌 영속성 컨텍스트 '1차 캐시'에서 조회해서 가져옴
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+
+            // 영속 엔티티의 동일성 보장
+            Member a = em.find(Member.class, 101L);
+            Member b = em.find(Member.class, 101L);
+            System.out.println("result = " + (a == b));
+
+            /**
+             *  배치
+             *  persistence.xml
+             *  <property name="hibernate.jdbc.batch_size" value="10"/>
+             */
+            Member memberA = new Member(150L, "A");
+            Member memberB = new Member(160L, "B");
+//            em.persist(memberA);
+//            em.persist(memberB);
+            System.out.println("=======================");
+
+            Member member1 = em.find(Member.class, 150L);
+            member1.setName("MBJ");
+
+            // 실제로 DB에 저장되는 시점
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
