ac9034e4d1c5bcf0fc4308b9fc63f28417506d6f
Jungma1 <mbjun3630@naver.com>
2022-06-18 22:32:39 +0900

    플러시, 준영속 상태

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,42 +15,27 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속 - 엔티티를 생성한 상태
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
-            System.out.println("==== BEFORE ====");
-            // 엔티티를 영속성 컨텍스트에 영속
+            /**
+             * 플러시
+             * - 영속성 컨텍스트를 비우지 않음
+             * - 영속성 컨텍스트의 변경 내용을 데이터베이스의 동기화
+             * - 트랜잭션이 있기 때문에 가능 -> 커밋 직전에만 동기화 하면 됨
+             */
+            Member member = new Member(200L, "member200");
             // em.persist(member);
-            System.out.println("==== AFTER ====");
-
-            // DB가 아닌 영속성 컨텍스트 '1차 캐시'에서 조회해서 가져옴
-            Member findMember = em.find(Member.class, 101L);
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getName() = " + findMember.getName());
-
-            // 영속 엔티티의 동일성 보장
-            Member a = em.find(Member.class, 101L);
-            Member b = em.find(Member.class, 101L);
-            System.out.println("result = " + (a == b));
+            // em.flush(); // 플러시 강제호출 - 쓰기 지연 SQL 저장소 바로 DB 반영, 1차 캐시는 유지
 
             /**
-             *  배치
-             *  persistence.xml
-             *  <property name="hibernate.jdbc.batch_size" value="10"/>
+             * 준영속 상태
+             * - 영속성 컨텍스트에서 분리, JPA 에서 더 이상 관리하지 않음
              */
-            Member memberA = new Member(150L, "A");
-            Member memberB = new Member(160L, "B");
-//            em.persist(memberA);
-//            em.persist(memberB);
-            System.out.println("=======================");
+            Member member1 = em.find(Member.class, 150L); // 영속
+            member1.setName("AAAAA");
+            // em.detach(member1); // 준영속 - update 쿼리문이 실행 안됨
+            em.clear();
 
-            Member member1 = em.find(Member.class, 150L);
-            member1.setName("MBJ");
-
-            // 실제로 DB에 저장되는 시점
-            tx.commit();
+            System.out.println("====================================");
+            tx.commit(); // 실제로 DB에 저장되는 시점
         } catch (Exception e) {
             tx.rollback();
         } finally {
