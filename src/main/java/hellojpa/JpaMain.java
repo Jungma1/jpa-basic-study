ee265a49225286358850e0339d483431a678db0b
Jungma1 <mbjun3630@naver.com>
2022-06-19 19:02:53 +0900

    필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,27 +15,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            /**
-             * 플러시
-             * - 영속성 컨텍스트를 비우지 않음
-             * - 영속성 컨텍스트의 변경 내용을 데이터베이스의 동기화
-             * - 트랜잭션이 있기 때문에 가능 -> 커밋 직전에만 동기화 하면 됨
-             */
-            Member member = new Member(200L, "member200");
-            // em.persist(member);
-            // em.flush(); // 플러시 강제호출 - 쓰기 지연 SQL 저장소 바로 DB 반영, 1차 캐시는 유지
+            Member member = new Member();
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
 
-            /**
-             * 준영속 상태
-             * - 영속성 컨텍스트에서 분리, JPA 에서 더 이상 관리하지 않음
-             */
-            Member member1 = em.find(Member.class, 150L); // 영속
-            member1.setName("AAAAA");
-            // em.detach(member1); // 준영속 - update 쿼리문이 실행 안됨
-            em.clear();
+            em.persist(member);
 
-            System.out.println("====================================");
-            tx.commit(); // 실제로 DB에 저장되는 시점
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -2,7 +2,13 @@ package hellojpa;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDateTime;
 
 @Entity
 public class Member {
@@ -10,16 +16,23 @@ public class Member {
     @Id
     private Long id;
 
-    @Column(unique = true, length = 10)
-    private String name;
+    @Column(name = "name", nullable = false)
+    private String username;
 
-    public Member() {
-    }
+    @Column
+    private Integer age;
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime lastModifiedDate;
+
+    @Lob
+    private String description;
 
     public Long getId() {
         return id;
@@ -29,11 +42,51 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
