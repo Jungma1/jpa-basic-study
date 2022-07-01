4281483c24a12c5378819b53cf03be7011983af4
Jungma1 <mbjun3630@naver.com>
2022-07-01 17:49:54 +0900

    프록시 확인 방법

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import org.hibernate.Hibernate;
 
 public class JpaMain {
 
@@ -29,8 +30,14 @@ public class JpaMain {
 
             // findMember = class hellojpa.Member$HibernateProxy$6wJulIUS
             System.out.println("findMember = " + findMember.getClass());
-            System.out.println("findMember.getId() = " + findMember.getId());
-            System.out.println("findMember.getUsername() = " + findMember.getUsername());
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            System.out.println("findMember.getUsername() = " + findMember.getUsername());
+
+            // 프록시 강제 초기화 *(JPA 표준은 강제 초기화 없음)
+            Hibernate.initialize(findMember);
+
+            // 프록시 인스턴스의 초기화 여부 확인
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(findMember));
 
             tx.commit();
         } catch (Exception e) {
