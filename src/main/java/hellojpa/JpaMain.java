d88b34e9db7cdb6c005e2b4311cae69eed8a4de0
Jungma1 <mbjun3630@naver.com>
2022-07-06 16:48:33 +0900

    객체 타입의 한계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -16,12 +15,25 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("Hello");
-            member.setWorkPeriod(new Period(LocalDateTime.now(), LocalDateTime.now()));
-            member.setHomeAddress(new Address("city", "street", "10000"));
+            Address address = new Address("city", "street", "10000");
 
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            member1.setHomeAddress(address);
+            em.persist(member1);
+
+            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+//            member2.setHomeAddress(address);
+            member2.setHomeAddress(copyAddress);
+            em.persist(member2);
+
+            // 임베디드 타입 같은 값 타입은 여러 엔티티에서 공유하면 위험함
+            // member1 에 city 정보를 변경했지만 member2 에 city 도 같이 변경됨
+            // 따라서, *값(인스턴스)를 복사해서 사용하도록 함
+            member1.getHomeAddress().setCity("newCity");
 
             tx.commit();
         } catch (Exception e) {
