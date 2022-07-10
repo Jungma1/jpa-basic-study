a7ba4afab8cad8125f9f0547c70f52b87ac3018c
Jungma1 <mbjun3630@naver.com>
2022-07-10 18:53:12 +0900

    Criteria

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,8 +4,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
-import java.util.Set;
 
 public class JpaMain {
 
@@ -17,47 +19,20 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setHomeAddress(new Address("city", "street", "10000"));
+            List<Member> result = em.createQuery("select m from d_member m where m.username like '%hello%'",
+                    Member.class).getResultList();
 
-            // 값 타입 컬렉션
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("피자");
-            member.getFavoriteFoods().add("족발");
-            member.getAddressHistory().add(new Address("old1", "street", "10000"));
-            member.getAddressHistory().add(new Address("old2", "street", "10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("========= START =========");
-            Member findMember = em.find(Member.class, member.getId());
-
-            // 값 타입 컬렉션도 지연 로딩 전략을 사용함
-            System.out.println("========= ADDRESS_HISTORY =========");
-            List<Address> addressHistory = findMember.getAddressHistory(); // 지연 로딩
-            for (Address address : addressHistory) {
-                System.out.println("address.getCity() = " + address.getCity());
+            for (Member member : result) {
+                System.out.println("member = " + member);
             }
 
-            System.out.println("========= FAVORITE_FOODS =========");
-            Set<String> favoriteFoods = findMember.getFavoriteFoods(); // 지연 로딩
-            for (String favoriteFood : favoriteFoods) {
-                System.out.println("favoriteFood = " + favoriteFood);
-            }
+            // Criteria (실무 사용 X) => 대신 QueryDSL 사용 권장
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-            System.out.println("========= 값 타입 수정 =========");
-            Address old = findMember.getHomeAddress();
-            findMember.setHomeAddress(new Address("newCity", old.getStreet(), old.getZipcode()));
-
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
-            findMember.getAddressHistory().add(new Address("newCity", "street", "10000"));
+            Root<Member> m = query.from(Member.class);
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "hello"));
+            List<Member> resultList = em.createQuery(cq).getResultList();
 
             tx.commit();
         } catch (Exception e) {
