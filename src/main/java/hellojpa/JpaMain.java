b3f4fbeae15ac76a30a027b680e4b9e8a937f68e
Jungma1 <mbjun3630@naver.com>
2022-07-15 17:52:43 +0900

    페치 조인

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,7 +6,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -19,49 +18,66 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("TeamA");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("TeamB");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("Member1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자1");
-            member2.setTeam(team);
+            member2.setUsername("Member2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("Member3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            // 상태 필드(state field): 경로 탐색의 끝, 탐색X
-            String query = "select m.username from j_member m";
-            List<String> result = em.createQuery(query, String.class)
+            // 지연 로딩
+            // Member1, TeamA(SQL)
+            // Member2, TeamA(영속성 컨텍스트 - 1차 캐시)
+            // Member3, TeamB(SQL)
+            // 만약, 회원이 100명이라면 쿼리문 실행이 많아짐 -> N + 1 문제 -> *페치 조인으로 해결
+            String query = "select m from j_member m";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
-            result.forEach(System.out::println);
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+            }
 
-            // 단일 값 연관 경로: *묵시적 내부 조인(inner join) 발생, 탐색O
-            String query2 = "select m.team from j_member m";
-            List<Team> result2 = em.createQuery(query2, Team.class)
+            // 페치 조인으로 회원과 팀을 함께 조회해서 지연 로딩이 없음
+            String query2 = "select m from j_member m join fetch m.team";
+            List<Member> result2 = em.createQuery(query2, Member.class)
                     .getResultList();
 
-            result2.forEach(System.out::println);
+            for (Member member : result2) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+            }
 
-            // 컬렉션 값 연관 경로: 묵시적 내부 조인 발생(실무X), 탐색X
-            String query3 = "select t.members from j_team t";
-            List<Collection> result3 = em.createQuery(query3, Collection.class)
+            // 컬렉션 페치 조인
+            String query3 = "select t from j_team t join fetch t.members";
+            List<Team> result3 = em.createQuery(query3, Team.class)
                     .getResultList();
 
-            System.out.println("result3 = " + result3);
+            for (Team team : result3) {
+                System.out.println("team = " + team.getName() + "|" + team.getMembers().size());
 
-            // 묵시적 조인 -> *명시적 조인을 사용: FROM 절에서 명시적 조인을 통해 별칭을 얻으면 별칭을 통해 탐색 가능
-            String query4 = "select m from j_team t join t.members m";
-            List<Member> result4 = em.createQuery(query4, Member.class)
-                    .getResultList();
-
-            result4.forEach(System.out::println);
+                for (Member member : team.getMembers()) {
+                    System.out.println("-> member = " + member);
+                }
+            }
 
             tx.commit();
         } catch (Exception e) {
