6d6005edbf441fad5f348e1b6496d6310641f02b
Jungma1 <mbjun3630@naver.com>
2022-07-15 22:14:21 +0900

    벌크 연산

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -6,7 +6,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -41,14 +40,21 @@ public class JpaMain {
             member3.setTeam(teamB);
             em.persist(member3);
 
-            em.flush();
-            em.clear();
+            /**
+             * 벌크 연산 - 쿼리 한 번으로 여러 테이블 로우 변경
+             * 벌크 연산 주의점 - 영속성 컨텍스트를 무시하고 데이터베이스에 직접 쿼리
+             * 해결책 - 1. 벌크 연산을 먼저 실행
+             *          2. 벌크 연산 수행 후 영속성 컨텍스트 초기화
+             */
+            int resultCount = em.createQuery("update j_member m set m.age = 20")
+                    .executeUpdate();
 
-            List<Member> members = em.createNamedQuery("j_member.findByUsername", Member.class)
-                    .setParameter("username", "Member1")
-                    .getResultList();
+            System.out.println("resultCount = " + resultCount);
 
-            members.forEach(System.out::println);
+            em.clear(); // 영속성 컨텍스트 초기화
+
+            Member findMember = em.find(Member.class, member1.getId()); // DB 에서 가져옴
+            System.out.println("findMember = " + findMember.getAge());
 
             tx.commit();
         } catch (Exception e) {
